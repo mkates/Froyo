1b496ee3de0e27003538b955805e6bfa2d9885de
Mitchell Kates <mhkates@gmail.com>
2012-05-30 13:46:54 -0400

    Django set up

--- cable/cable/urls.py
+++ cable/cable/urls.py
@@ -1,17 +1,23 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns('cablefinder.views',
     # Examples:
     # url(r'^$', 'cable.views.home', name='home'),
-    # url(r'^cable/', include('cable.foo.urls')),
-
+    #url(r'^cable/', include('cable.foo.urls')),
+	
+	url(r'^$', 'index'),
+    
+    #url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+    #url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+    #url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+	
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
