4089bf3951a328853de0e1de32715ed96e36cad0
Mitchell Kates <mhkates@gmail.com>
2012-06-06 19:04:05 -0400

    Worked on models a little

--- cable/cablefinder/models.py
+++ cable/cablefinder/models.py
@@ -5,19 +5,46 @@ from django.db import models
 class Company(models.Model):
 	name = models.CharField(max_length=200)
 
+#Channels like HBO, Stars, etc.
+class Channel(models.Model):
+	name = models.CharField(max_length=100)
+
+#Promotions, in the form of text
+class Promotion(models.Model):
+	name = models.CharField(max_length=500)
+
 # A package offered by a company
 class Package(models.Model):
 	SERVICE_TYPES = (('i','internet'),('t','tv'),('p','phone'))
-	
 	company = models.ForeignKey(Company)
 	name = models.CharField(max_length=200)
-	service = models.CharField(choices=SERVICE_TYPES,max_length = 1)
+	service = models.CharField(choices=SERVICE_TYPES, max_length = 1)
+
+class SatellitePackage(models.Model):
+	name = models.CharField(max_length=100)
+	basePrice = models.FloatField()
+	promotions = models.ManyToManyField(Promotion)
 	
-#A combination of several packages
-class Bundle(models.Model):
-	name = models.CharField(max_length=200)
-
-
-
-
-
+	totalChannels =  models.IntegerField()
+	channels = models.ManyToManyField(Channel)
+	costInitial = models.FloatField()
+	onlineOrderSavings = models.IntegerField()
+	
+#UNUSED CLASSES RIGHT NOW
+class User(models.Model):
+	address = models.CharField(max_length=200, null=True)
+	zipcode = models.IntegerField(length=5, null=True)
+	latitude = models.FloatField(null=True)
+	longitude = models.FloatField(null=True)
+	
+	SERVICES_WANTED = (('tpi','tv/phone/internet'),
+						('tp','tv/phone'),
+						('ti','tv/internet'),
+						('pi','phone/internet'),
+						('t','tv'),
+						('p','phone'),
+						('i','internet'))
+	services = models.CharField(choices=SERVICES_WANTED, max_length = 3)
+	
+	
+	
--- cable/cablefinder/views.py
+++ cable/cablefinder/views.py
@@ -7,11 +7,8 @@ import simplejson
 
 
 def index(request):
-	print search("02215","99 Bay State Road")
+	print search("07722","10 Pacer Court")
 	return render_to_response('index.html',context_instance=RequestContext(request))
-	
-class YahooSearchError(Exception):
-    pass
 
 #Yahoo Search Parameters
 APP_ID = "V7xEqm32"
@@ -20,27 +17,29 @@ SEARCH_BASE = "http://where.yahooapis.com/geocode?"
 def search(zipcode,address):
 	#Yahoo geocoder
 	try:
-		params = urllib.urlencode({'postal':zipcode,'line1':address,'flags':"J","appid":APP_ID})
-		string = SEARCH_BASE + params
-		result = simplejson.load(urllib.urlopen(string))
-		if 'Error' in result:
-			raise YahooSearchError, result['Error']
-		print result['ResultSet']['Results'][0]
+		#Zip Code to city state
+		params = urllib.urlencode({'postal':zipcode,'country':'USA','flags':"J","appid":APP_ID})
+		result = simplejson.load(urllib.urlopen(SEARCH_BASE + params))
+		line2 = result['ResultSet']['Results'][0]['line2']
+		
+		#Convert address,zip,city,state to lat and long
+		params = urllib.urlencode({'postal':zipcode,'addr':address,'line2':line2,'flags':"J","appid":APP_ID})
+		result = simplejson.load(urllib.urlopen(SEARCH_BASE + params))
 		latitude = result['ResultSet']['Results'][0]['latitude']
 		longitude = result['ResultSet']['Results'][0]['longitude']
-		print latitude
-		print longitude
 	except:
 		return "Error"
-	
 	try:
+		#National Broadband API Call for wireless
 		string = "http://www.broadbandmap.gov/broadbandmap/broadband/jun2011/wireless?"
 		params = urllib.urlencode({"latitude":latitude,"longitude":longitude,"format":"json"})
 		result = simplejson.load(urllib.urlopen(string+params))
 		result = result['Results']['wirelessServices']
+		companies = []
 		for i in result:
-			print i["doingBusinessAs"]
+			if i != None:
+				companies.append(i["doingBusinessAs"])
+		return companies
 	except:
 		return "Error"
-#National Broadband API Call
     
